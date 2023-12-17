0354c4a85fb9405d6b76e630a36ce6d798c6afff
Xawas <Ninja2133@protonmail.com>
2023-12-17 22:47:22 +0100

    starting new movement system

--- Assets/Scripts/DragonMovement.cs
+++ Assets/Scripts/DragonMovement.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class DragonMovement : MonoBehaviour
 {
+    [SerializeField] float curSpeed;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float accelerationForce;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] PlayerInput movementAction;
+    [SerializeField] Vector2 directions;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        curSpeed = rb.velocity.magnitude;
+        if(curSpeed >= maxSpeed)
+        {
+            curSpeed = maxSpeed;
+            return;
+        }
+        rb.AddForce(directions.y * transform.forward * accelerationForce,ForceMode.Acceleration);
         
+    }
+    public void GetDirections(InputAction.CallbackContext callbackContext)
+    {
+        directions = callbackContext.ReadValue<Vector2>();
     }
 }
