b0e362138f52c5c152d11759f936eb495322dc3c
Xawas <Ninja2133@protonmail.com>
2023-12-22 22:13:58 +0100

    i m stuck at flying

--- Assets/Scripts/DragonMovement.cs
+++ Assets/Scripts/DragonMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class DragonMovement : MonoBehaviour
@@ -10,19 +11,118 @@ public class DragonMovement : MonoBehaviour
     [SerializeField] Rigidbody rb;
     [SerializeField] PlayerInput movementAction;
     [SerializeField] Vector2 directions;
+    [SerializeField] float wingTime;
+    [SerializeField] float curTime;
+    [SerializeField] float jumptForce;
+    [SerializeField] float currentChangeFlyTime;
+    [SerializeField] bool isGrounded;
+    [SerializeField] Transform groundCheckTransform;
+    [SerializeField] float groundCheckRadius;
+    [SerializeField] LayerMask groundLayerMask;
+    [SerializeField] bool isEvalute;
+    [SerializeField] AnimationCurve increasingCurveY;
+    [SerializeField] AnimationCurve fallingCurveY;
+    private AnimationCurve currentAnimationCurve;
+    
     // Start is called before the first frame update
     
-
+    private void Start() {
+        curTime = wingTime;
+        
+    }
     // Update is called once per frame
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(groundCheckTransform.position,groundCheckRadius);
+    }
+    private void Update() {
+        ChangeFly(currentAnimationCurve);
+        if(rb.velocity.y <= 0 && !isGrounded)
+        {
+            rb.constraints = RigidbodyConstraints.FreezePositionY;
+        }
+    }
+    void ChangeFly(AnimationCurve curve)
+    {
+        if (isEvalute)
+                {
+                    currentChangeFlyTime += Time.deltaTime;
+
+                    rb.velocity = new Vector3(0, jumptForce +  curve.Evaluate(currentChangeFlyTime), 0);
+
+                    // Assuming curveY is not empty
+                    float lastKeyframeTime = curve.keys[curve.length - 1].time;
+                    
+                    if (lastKeyframeTime <= currentChangeFlyTime)
+                    {
+                        rb.velocity = Vector3.zero;
+                        isEvalute = false;
+                        // Perform actions when the animation reaches its end
+                        // Example: rb.constraints = RigidbodyConstraints.FreezePositionY;
+                    }
+                }
+    }
+    AnimationCurve changeAnimation(int id)
+    {
+        switch(id)
+        {
+            case 0:
+                return increasingCurveY;
+            break;
+            case 1:
+                return fallingCurveY;
+            break;
+        }
+        return null;
+        
+    }
     void FixedUpdate()
     {
-        curSpeed = rb.velocity.magnitude;
-        if(curSpeed >= maxSpeed)
+        
+        isGrounded = Physics.CheckSphere(groundCheckTransform.position,groundCheckRadius,groundLayerMask);
+        
+        if(directions.magnitude != 0)
         {
-            curSpeed = maxSpeed;
-            return;
+            
+            curTime -= Time.deltaTime;
+            if(curTime <= 0)
+            {
+                
+                curSpeed = rb.velocity.magnitude;
+                if(curSpeed >= maxSpeed)
+                {
+                    
+                    curSpeed = maxSpeed;
+                    return;
+                }
+                
+                rb.AddForce(directions.y * transform.forward * accelerationForce,ForceMode.Acceleration);
+                curTime = wingTime;
+            }
+            
+            
         }
-        rb.AddForce(directions.y * transform.forward * accelerationForce,ForceMode.Acceleration);
+        
+        
+    }
+    public void ChangeFly(InputAction.CallbackContext callback)
+    {
+        print("D");
+        if(callback.started && isGrounded)
+        {
+            currentChangeFlyTime = 0;
+            changeAnimation(0);
+            isEvalute = true;
+            
+            
+        }
+        else if(callback.started && !isGrounded)
+        {
+            currentChangeFlyTime = 0;
+            changeAnimation(1);
+            isEvalute = true;
+        }
+        
+        
         
     }
     public void GetDirections(InputAction.CallbackContext callbackContext)
