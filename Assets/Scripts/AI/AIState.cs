b3e7025be832ef4f915fe482e5d7e0f5208b3096
Xawas <Ninja2133@protonmail.com>
2024-02-01 09:55:53 +0100

    segregate all to folders

--- Assets/Scripts/AI/AIState.cs
+++ Assets/Scripts/AI/AIState.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.Android;
 public enum AiStateId
 {
     ChasePlayer,Idle,Shooting,MeleeAttack,Burn
---
+++ Assets/Scripts/UI/DisplaySkillStatistic.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+public class DisplaySkillStatistic : MonoBehaviour
+{
+    [SerializeField] TMP_Text skillNameText;
+    [SerializeField] TMP_Text skillDescryptionText;
+    public static DisplaySkillStatistic instance;
+    private void Awake() {
+        // Ensure there is only one instance
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+    private void Start() {
+        skillNameText = GameObject.Find("SkillName").GetComponent<TMP_Text>();
+        skillDescryptionText = GameObject.Find("SkillDescryption").GetComponent<TMP_Text>();
+    }
+    public void DisplayStatistic(string _skillName, string _skillDescryption)
+    {
+        skillNameText.text = _skillName;
+        skillDescryptionText.text = _skillDescryption;
+    }
+}
